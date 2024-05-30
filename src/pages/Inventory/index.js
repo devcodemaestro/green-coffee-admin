1a89bfdca217d152084e3fc09401f302bd28154e
devcodemaestro <dev.codemaestro@gmail.com>
2024-05-30 11:08:18 +0900

    feat : 메뉴관리 더미 작업

--- src/pages/Inventory/index.js
+++ src/pages/Inventory/index.js
@@ -1,10 +1,65 @@
-import { Typography } from "antd/es";
+import { Avatar, Rate, Space, Table, Typography } from "antd/es";
+import { useEffect, useState } from "react";
+import { getInventory } from "../../api";
 
 function Inventory() {
+  const [loading, setLoading] = useState(false);
+  const [dataSoure, setDataSource] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    getInventory(setDataSource);
+    setLoading(false);
+  }, []);
+
   return (
-    <div>
+    <Space size={20} direction="vertical">
       <Typography.Title level={4}>메뉴관리</Typography.Title>
-    </div>
+      <Table
+        loading={loading}
+        columns={[
+          {
+            title: <div style={{ textAlign: "center" }}>상품</div>,
+            dataIndex: "thumbnail",
+            render: (link) => {
+              return <Avatar src={link} />;
+            },
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>품명</div>,
+            dataIndex: "title",
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>가격</div>,
+            dataIndex: "price",
+            render: (value) => <span>{value}원</span>,
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>추천</div>,
+            dataIndex: "rating",
+            render: (rating) => {
+              return <Rate value={rating} allowHalf disabled />;
+            },
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>판매점</div>,
+            dataIndex: "stock",
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>브랜드</div>,
+            dataIndex: "brand",
+          },
+          {
+            title: <div style={{ textAlign: "center" }}>카테고리</div>,
+            dataIndex: "category",
+          },
+        ]}
+        dataSource={dataSoure}
+        pagination={{
+          pageSize: 7,
+        }}
+      ></Table>
+    </Space>
   );
 }
 
